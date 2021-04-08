951c4adc94351870d0f5d7577646510dcf393544
Joe Palala <jose.palala@live.com>
2021-04-08 08:14:59 +0800

    CHOIR: remove examples update tslint

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
 import maya from '@mayajs/router';
 import http from 'http';
 import { Logger } from 'sitka';
-
 const PORT = 3000;
 const app = maya();
-
-app.add([
-  {
-    path: "/",
-    GET: () => "Hello World!",
-  },
-]);
+app.add([{
+  path: '/',
+  GET: () => 'Hello World!',
+}]);
 
 http.createServer(app).listen(PORT, () => {
-    const newLogger = Logger.getLogger({ name: 'Server' });
-    newLogger.debug(`Server listening on port ${PORT}.`)
+  const newLogger = Logger.getLogger({ name: 'Server' });
+  newLogger.debug(`Server listening on port ${PORT}.`)
 });
